20a4893e756858e093a97a4377b6585cfc2c6238
tamakiii <tamakiii@users.noreply.github.com>
2023-08-07 20:36:05 +0900

    init main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+mod args {
+    use clap::Parser;
+
+    mod generate {
+        use clap::Parser;
+
+        #[derive(Parser)]
+        pub struct Arguments {
+            pub path: std::path::PathBuf,
+        }
+    }
+
+    #[derive(Parser)]
+    pub enum Command {
+        Generate(generate::Arguments),
+    }
+
+    #[derive(Parser)]
+    pub struct Arguments {
+        #[clap(subcommand)]
+        pub command: Command,
+    }
+
+    pub fn parse() -> Arguments {
+        Arguments::parse()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = args::parse();
+
+    match args.command {
+        args::Command::Generate(args) => println!("path: {}", args.path.to_str().unwrap()),
+    };
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn parse_args_ok() {
+        // Arguments::parse
+    }
 }
