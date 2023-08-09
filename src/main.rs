5a6325f160fb4759d7dce00251509c54bf5d1665
tamakiii <tamakiii@users.noreply.github.com>
2023-08-09 20:02:25 +0900

    impl port https://docs.rs/clap/latest/clap/_derive/_tutorial/index.html#validated-values

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,26 @@ mod args {
         #[derive(Parser)]
         pub struct Arguments {
             pub path: std::path::PathBuf,
+
+            #[arg(value_parser = port_in_range)]
+            pub port: u16,
+        }
+
+        const PORT_RANGE: std::ops::RangeInclusive<usize> = 1..=65535;
+
+        fn port_in_range(s: &str) -> Result<u16, String> {
+            let port = s
+                .parse()
+                .map_err(|_| format!("`{s}` isn't a port number"))?;
+            if PORT_RANGE.contains(&port) {
+                Ok(port as u16)
+            } else {
+                Err(format!(
+                    "port not in range {}-{}",
+                    PORT_RANGE.start(),
+                    PORT_RANGE.end()
+                ))
+            }
         }
     }
 
@@ -30,7 +50,9 @@ fn main() {
     let args = args::parse();
 
     match args.command {
-        args::Command::Generate(args) => println!("path: {}", args.path.to_str().unwrap()),
+        args::Command::Generate(args) => {
+            println!("path: {}, port: {}", args.path.to_str().unwrap(), args.port)
+        }
     };
 }
 
